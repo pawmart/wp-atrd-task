7d3d3fe55f5b4cbb23ea211c2c34190ccecd1e7a
Alex <32b3@protonmail.com>
2020-12-03 22:09:40 +0100

    main file and dependencies updated

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"github.com/alkmc/wp-atrd-task/internal/controller"
+	"github.com/alkmc/wp-atrd-task/internal/repository"
+	"github.com/alkmc/wp-atrd-task/internal/router"
+	"github.com/alkmc/wp-atrd-task/internal/service"
+	"github.com/alkmc/wp-atrd-task/internal/validator"
+)
 
+var (
+	secretRepository = repository.NewRedis()
+	secretService    = service.NewService(secretRepository)
+	secretValidator  = validator.NewValidator()
+	secretController = controller.NewController(secretService, secretValidator)
+	secretRouter     = router.NewChiRouter()
+)
+
+func main() {
+	mapUrls()
+	defer secretRepository.CloseDB()
+	secretRouter.SERVE()
+}
+
+func mapUrls() {
+	secretRouter.GET("/v1/secret/{hash}", secretController.GetSecretByHash)
+	secretRouter.POST("/v1/secret", secretController.AddSecret)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,12 @@
 module github.com/alkmc/wp-atrd-task
 
 go 1.15
+
+require (
+	github.com/go-chi/chi v1.5.0
+	github.com/go-redis/redis v6.15.9+incompatible
+	github.com/google/uuid v1.1.2
+	github.com/onsi/ginkgo v1.14.2 // indirect
+	github.com/onsi/gomega v1.10.3 // indirect
+	github.com/stretchr/testify v1.6.1
+)
