dfc0aa5a65e47d75551229bdf916ae149ef1254f
Piotr Kołodyński <hehlolrofl@gmail.com>
2021-03-03 09:23:52 +0100

    Moved config logic to its own package, added default values there

--- NotSoSecretSecret/cmd/secret-server/main.go
+++ NotSoSecretSecret/cmd/secret-server/main.go
@@ -6,37 +6,23 @@ import (
 	"log"
 	"net/http"
 	"notsosecretsercet/pkg/adding"
+	"notsosecretsercet/pkg/config"
 	"notsosecretsercet/pkg/http/rest"
 	"notsosecretsercet/pkg/listing"
 	"notsosecretsercet/pkg/storage/mongostore"
 	"time"
 
-	"github.com/spf13/viper"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
-
-func init() {
-	viper.SetConfigName("config")
-	viper.SetConfigType("yaml")
-	viper.AddConfigPath("./cmd/secret-server/config/")
-	viper.AddConfigPath("/cmd/secret-server/config/")
-	viper.AddConfigPath("./config")
-
-	err := viper.ReadInConfig()
-
-	if err != nil {
-		log.Fatalf("%v", err)
-	}
-}
 
 func main() {
 
 	ctx, cancel := context.WithTimeout(context.Background(), 7*time.Second)
 	defer cancel()
 
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI(viper.GetString("DB_URI")))
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(config.DbUri))
 
 	if err != nil {
 		log.Fatalf("Unable to connect to database. Secret server shuts down :(")
@@ -51,7 +37,7 @@ func main() {
 		log.Fatalf("Unable to connect to database. Secret server shuts down :(")
 	}
 
-	db := client.Database(viper.GetString("DB_NAME"))
+	db := client.Database(config.DbName)
 
 	repo := mongostore.NewStorage(db)
 	addingService := adding.NewService(repo)
